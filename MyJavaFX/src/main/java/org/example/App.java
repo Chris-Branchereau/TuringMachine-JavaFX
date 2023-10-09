4473e0fe632c8f10d83870805803c30001b827d8
60127 <60127@etu.he2b.be>
2023-10-09 15:43:51 +0200

    TD3 - 7

--- MyJavaFX/src/main/java/org/example/App.java
+++ MyJavaFX/src/main/java/org/example/App.java
@@ -1,10 +1,21 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -21,6 +32,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        /* Chap 2
         primaryStage.setTitle("My First JavaFX App");
         //primaryStage.initStyle(StageStyle.TRANSPARENT);
 
@@ -30,6 +43,204 @@ public class App extends Application {
         helloText.setFill(Color.RED);
         root.setTop(helloText);
         Scene scene = new Scene(root, 500, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+         */
+
+
+
+        /* HelloWorldProperty
+        primaryStage.setTitle("My First JavaFX App");
+        BorderPane root = new BorderPane();
+
+        Label helloText = new Label("Hello World");
+        helloText.setUnderline(true);
+        helloText.setTextFill(Color.RED);
+        helloText.setFont(Font.font("Verdana", 20));
+
+        System.out.println("Le message du Libellé est " + helloText.getText());
+        System.out.println("La police du Libellé est " + helloText.getFont());
+        System.out.println("La couleur du Libellé est " + helloText.getTextFill());
+
+        root.setCenter(helloText);
+        Scene scene = new Scene(root, 250, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        */
+
+
+
+        /* HelloWorldCheckBox
+        primaryStage.setTitle("My First JavaFX App");
+        BorderPane root = new BorderPane();
+
+        CheckBox checkBox1 = new CheckBox();
+        checkBox1.setText("First");
+        checkBox1.setSelected(true);
+
+        CheckBox checkBox2 = new CheckBox("Second");
+        checkBox2.setIndeterminate(true);
+
+        CheckBox checkBox3 = new CheckBox("Third");
+
+        checkBox3.setAllowIndeterminate(true);
+
+        //Alignment
+        root.setLeft(checkBox1);
+        //BorderPane.setAlignment(checkBox1, Pos.CENTER);
+        root.setCenter(checkBox2);
+        root.setRight(checkBox3);
+        //BorderPane.setAlignment(checkBox3, Pos.CENTER);
+
+        Scene scene = new Scene(root, 250, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        */
+
+
+
+        /* HelloWorldTextField
+        primaryStage.setTitle("My First JavaFX App");
+        BorderPane root = new BorderPane();
+
+        Label userName = new Label("User Name");
+
+        PasswordField tfdUserName = new PasswordField();
+        tfdUserName.setPrefColumnCount(12);
+        tfdUserName.setAlignment(Pos.CENTER_LEFT);
+
+        //Alignment
+        root.setTop(userName);
+        BorderPane.setAlignment(userName, Pos.CENTER);
+        root.setCenter(tfdUserName);
+
+        Scene scene = new Scene(root, 250, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        */
+
+
+
+        /* HelloWorldTextFieldAction
+        primaryStage.setTitle("My First JavaFX App");
+        BorderPane root = new BorderPane();
+
+        Label userName = new Label("User Name");
+        Label test = new Label("User name saved! You can’t change it");
+
+        TextField tfdUserName = new TextField();
+        tfdUserName.setPrefColumnCount(12);
+        tfdUserName.setAlignment(Pos.CENTER_LEFT);
+        tfdUserName.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                root.setBottom(test);
+                BorderPane.setAlignment(test, Pos.CENTER);
+                tfdUserName.setEditable(false);
+                tfdUserName.setAlignment(Pos.CENTER);
+            }
+        });
+        //Alignment
+        root.setTop(userName);
+        BorderPane.setAlignment(userName, Pos.CENTER);
+        root.setCenter(tfdUserName);
+
+        Scene scene = new Scene(root, 300, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        */
+
+
+
+        /* HelloWorldTextArea
+        primaryStage.setTitle("My First JavaFX App");
+        BorderPane root = new BorderPane();
+        TextArea txaUserName = new TextArea();
+        txaUserName.setPrefColumnCount(12);
+        txaUserName.setPrefRowCount(5);
+        txaUserName.setWrapText(true);
+
+        Button btnPrint = new Button("Print");
+        btnPrint.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(txaUserName.getText());
+            }
+        });
+        //Alignment
+        root.setTop(txaUserName);
+        BorderPane.setAlignment(txaUserName, Pos.CENTER);
+        root.setCenter(btnPrint);
+
+        Scene scene = new Scene(root, 250, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        */
+
+
+
+
+        /* HBoxSample and  VBoxSample
+        primaryStage.setTitle("My First JavaFX App");
+        VBox root = new VBox(10);
+        //root.setAlignment(Pos.CENTER);
+
+        CheckBox checkBox1 = new CheckBox();
+        checkBox1.setText("First");
+        checkBox1.setSelected(true);
+
+        CheckBox checkBox2 = new CheckBox("Second");
+        checkBox2.setIndeterminate(true);
+
+        CheckBox checkBox3 = new CheckBox("Third");
+
+        checkBox3.setAllowIndeterminate(true);
+
+        root.getChildren().addAll(checkBox1, checkBox2, checkBox3);
+        //root.getChildren().add(checkBox2);
+        //root.getChildren().add(checkBox3);
+
+        Scene scene = new Scene(root, 250, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        */
+
+
+
+
+        /* GridPane */
+        primaryStage.setTitle("My First JavaFX App");
+        GridPane root = new GridPane();
+        root.setPadding(new Insets(10));
+        root.setHgap(10);
+        root.setVgap(5);
+
+        Label lblTitle = new Label("JavaFX Course Login");
+        lblTitle.setFont(Font.font("System", FontWeight.BOLD, 20));
+        lblTitle.setTextFill(Color.RED);
+        root.add(lblTitle, 0, 0, 2, 1);
+        GridPane.setHalignment(lblTitle, HPos.CENTER);
+        GridPane.setMargin(lblTitle, new Insets(0, 0, 10, 0));
+
+        Label lblUserName = new Label("User Name or email");
+        GridPane.setHalignment(lblUserName, HPos.RIGHT);
+        root.add(lblUserName, 0, 1);
+
+        TextField tfdUserName = new TextField();
+        tfdUserName.setPrefColumnCount(20);
+        root.add(tfdUserName, 1, 1);
+
+        Label lblPassword = new Label("Password");
+        root.add(lblPassword, 0, 2);
+
+        TextField tfdPassword = new TextField();
+        tfdPassword.setPrefColumnCount(12);
+        root.add(tfdPassword, 1, 2);
+
+        GridPane.setHalignment(lblPassword, HPos.RIGHT);
+        GridPane.setFillWidth(tfdPassword, false);
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
---
+++ REPONSES.md
@@ -0,0 +1,148 @@
+# ATLG3 - TD2
+
+## 1
+### 1.1
+
+
+    (3.0, 6.0) - FF0000FF
+    x: 3.0
+    color : FF0000FF
+
+
+
+### 1.2
+
+
+    java: cannot find symbol
+        symbol:   method getColor()
+            location: variable p of type be.g60127.td2.Point
+
+la ligne 10.    
+non, car simplement, il n'y a pas de fonction getColor() dans Point.
+
+
+
+### 1.3
+
+non, car Point est le parent de ColoredPoint
+
+### 1.4
+non, car x et y sont private dans la classe parente 
+
+### 1.5
+    java: cyclic inheritance
+
+### 1.6
+    java: cannot inherit from final
+car, on ne peut pas hériter d'une classe final
+
+## 2
+### 2.1
+Oui, car la classe Object est parente à toute classe en java.
+
+### 2.2
+Oui, même raison que 2.1
+
+### 2.3
+Oui, car cette méthode vient de la classe Object et est publique. 
+
+## 3 
+### 3.1
+    java: call to super must be first statement in constructor
+### 3.2
+    java: constructor Point in class be.g60127.td2.Point cannot be applied to given types;
+        required: double,double
+        found:    no arguments
+        reason: actual and formal argument lists differ in length
+Elle sert à hériter du constructeur de la classe Parent : Point
+
+### 3.3
+Non
+
+## 4
+### 4.1
+    constructor of A
+    constructor of B
+    constructor of C
+
+### 4.2
+    constructor of A
+    constructor of B
+
+### 4.4
+Il n'y a qu'un constructeur vide
+
+
+## 5
+### 5.1
+    (0.0, 0.0) - not pinned
+    (1.0, 1.0) - pinned
+### 5.2
+Celle de PinnablePoint 
+### 5.3
+    java: move(double,double) in be.g60127.td2.PinnablePoint cannot override move(double,double) in be.g60127.td2.Point
+    overridden method does not throw java.lang.Exception
+On ne peut pas l'override si celle de Point n'est pas modifié avec Exception
+
+### 5.4
+Non
+
+# ATLG3 - TD3
+
+##  Question 1
+### 1.1
+Cela change la taille de la scène
+
+### 1.2
+Cela retire les bords de la
+
+    StageStyle.DECORATED
+    StageStyle.UNDECORATED
+    StageStyle.TRANSPARENT
+    StageStyle.UTILITY
+
+### 1.3
+Cela place l'élément sur les différentes parties de la scène (Haut, bas, gauche, droite)
+
+
+## Question 2
+### 2.1
+First : Il est déjà sélectionné au début
+
+Second : Il est indéterminé uniquement au début (Il ne peut plus l'être une fois changer)
+
+Third : Il est désélectionné au début et permet l'indétermination
+
+### 2.2
+First : se place en haut à gauche (plus au centre)
+
+Second : se place en haut à droite (idem)
+
+## Question 3
+### 3.1
+Cela cache les caractères
+
+### 3.2
+La zone de texte n'est plus modifiable et le texte est centré
+
+## Question 4
+Le texte présent dans la zone est affiché dans la console
+
+## Question 5
+    ObservableList<Node>
+
+Grouper Les "nodes" dans une liste
+
+## Question 6
+### 6.3
+Il va falloir redimensionné les éléments nous même s'ils dépassent la taille de la boite
+
+    setClip(Node)
+
+## Question 7
+
+
+
+
+
+
